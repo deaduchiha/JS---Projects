110d66981334a1db16be9f549c9b88929a73a66f
Alireza Nikzad <alireza.nikzdd@gmail.com>
2024-01-14 19:37:04 +0330

    add fake data

---
+++ 1/assets/scripts/fakeData.js
@@ -0,0 +1,42 @@
+const fakeData = [
+  {
+    shoe: "Air Jordan 1 Retro High OG",
+    price: 180,
+    image: "../images/Air Jordan 1 Retro High OG.jpeg",
+  },
+  {
+    shoe: "Nike Metcon 9 AMP",
+    price: 160,
+    image: "../images/Nike Metcon 9 AMP.png",
+  },
+  {
+    shoe: "Nike Dunk Low",
+    price: 120,
+    image: "../images/Nike Dunk Low.png",
+  },
+  {
+    shoe: "SAMBA OG SHOES",
+    price: 90,
+    image: "../images/SAMBA OG SHOES.avif",
+  },
+  {
+    shoe: "GALAXY 5 SHOES",
+    price: 42.95,
+    image: "../images/GALAXY 5 SHOES.avif",
+  },
+  {
+    shoe: "RUN FALCON",
+    price: 45,
+    image: "../images/RUN FALCON 2.0 SHOES.avif",
+  },
+  {
+    shoe: "Palermo Lth",
+    price: 75,
+    image: "../images/Palermo-Lth.avif",
+  },
+  {
+    shoe: "ASICS GEL Nimbus 25",
+    price: 174,
+    image: "../images/ASICS GEL Nimbus 25.jpeg",
+  },
+];
