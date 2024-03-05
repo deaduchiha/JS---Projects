f4f464adb4061e3bdb4f3957a1d60406de071cf9
Alireza Nikzad <alireza.nikzdd@gmail.com>
2024-03-05 16:46:13 +0330

    feat: fetch data from api

--- 3/assets/js/game.js
+++ 3/assets/js/game.js
@@ -4,11 +4,30 @@
 // BASE_URL-> https://opentdb.com ---- end point -> /api.php ---- queries -> ?amount=10&difficulty=medium&type=multiple
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+let formattedData = null;
+
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
+const formatData = (questionData) => {
+  console.log(questionData);
+};
 
 const fetchData = async () => {
-  const res = await fetch(URL);
-  const data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch(URL);
+    const data = await res.json();
+    formattedData = data;
+    formatData(data.results);
+    start(); // we are calling start function to make loader none and show container
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const start = () => {
+  loader.style.display = "none";
+  container.style.display = "block";
 };
 
 window.addEventListener("load", fetchData);
