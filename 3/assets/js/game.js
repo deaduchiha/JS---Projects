7f8b03e3e9b453e972a33a467655daf63d5598fc
Alireza Nikzad <alireza.nikzdd@gmail.com>
2024-03-06 17:54:29 +0330

    feat: show question and answers from api

--- 3/assets/js/game.js
+++ 3/assets/js/game.js
@@ -6,9 +6,13 @@ import formatData from "./helper.js";
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 let formattedData = null; // for having global variable of our data
+let questionIndex = 0;
+let correctAnswer = null;
 
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 
 const fetchData = async () => {
   try {
@@ -17,13 +21,30 @@ const fetchData = async () => {
     formattedData = formatData(data.results);
     start(); // we are calling start function to make loader none and show container
   } catch (error) {
-    console.log(error);
+    console.log("we have error: ", error);
   }
 };
 
 const start = () => {
+  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
 };
 
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } =
+    formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerHTML = question;
+
+  answerList.forEach((button, index) => {
+    button.innerHTML = answers[index];
+  });
+};
+
+const checkAnswer = () => {};
+
 window.addEventListener("load", fetchData);
+answerList.forEach((button, index) => {
+  button.addEventListener("click", checkAnswer);
+});
