0596c7e11cc286040f12611532e216137f2739c3
Alireza Nikzad <alireza.nikzdd@gmail.com>
2024-03-06 19:33:14 +0330

    feat: add to handle hight scores with sorting them

---
+++ 3/assets/js/end.js
@@ -0,0 +1,26 @@
+const score = localStorage.getItem("score");
+const scoreEle = document.querySelector("p");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+scoreEle.innerText = score;
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const saveHandler = () => {
+  if (!input.value || !score) {
+    alert("Invalid username or score");
+  } else {
+    const finalScore = {
+      name: input.value,
+      score,
+    };
+
+    highScores.push(finalScore);
+    highScores.sort((a, b) => b.score - a.score);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    localStorage.removeItem("scores");
+    window.location.assign("/index.html");
+  }
+};
+
+button.addEventListener("click", saveHandler);
