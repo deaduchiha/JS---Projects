e4a5325fb3715b6b6691623d6649d4ee2f516f3c
Alireza Nikzad <alireza.nikzdd@gmail.com>
2024-03-06 18:21:19 +0330

    feat: handle to getting score for each question

--- 3/assets/js/game.js
+++ 3/assets/js/game.js
@@ -3,16 +3,20 @@ import formatData from "./helper.js";
 // we have queries too -> what is queries?
 // this is our API => https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple
 // BASE_URL-> https://opentdb.com ---- end point -> /api.php ---- queries -> ?amount=10&difficulty=medium&type=multiple
+const CORRECT_BONUS = 10;
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 let formattedData = null; // for having global variable of our data
 let questionIndex = 0;
 let correctAnswer = null; // we need it to apply correct answer in our function
+let score = 0;
+let isAccepted = true;
 
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
 const answerList = document.querySelectorAll(".answer-text");
+const scoreText = document.getElementById("score");
 
 const fetchData = async () => {
   try {
@@ -44,10 +48,14 @@ const showQuestion = () => {
 };
 
 const checkAnswer = (event, index) => {
-  const isCorrect = index === correctAnswer ? true : false;
+  if (!isAccepted) return;
+  isAccepted = false;
 
+  const isCorrect = index === correctAnswer ? true : false;
   if (isCorrect) {
     event.target.classList.add("correct");
+    score += CORRECT_BONUS;
+    scoreText.innerHTML = score;
   } else {
     event.target.classList.add("incorrect");
     answerList[correctAnswer].classList.add("correct");
