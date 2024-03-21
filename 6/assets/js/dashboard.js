c8639849bcb9581c85783d71d2f3496ef071e0f1
Alireza Nikzad <alireza.nikzdd@gmail.com>
2024-03-21 13:27:30 +0330

    feat:  showing user details

--- 6/assets/js/dashboard.js
+++ 6/assets/js/dashboard.js
@@ -5,6 +5,40 @@ const mainContainer = document.getElementById("container");
 
 const renderUsers = (users) => {
   mainContainer.innerHTML = "";
+
+  users.forEach((user) => {
+    const jsx = `
+        <div id="card">
+            <h3>${user.id}</h3>
+
+            <div>
+                <p> <i class="fa-solid fa-user"></i> Name: </p>
+                <span>${user.name.firstname} ${user.name.lastname}</span>
+            </div>
+
+            <div>
+                <p> <i class="fa-solid fa-paperclip"></i> username: </p>
+                <span>${user.username}</span>
+            </div>
+
+            <div>
+                <p> <i class="fa-solid fa-envelope"></i> email: </p>
+                <span>${user.email}</span>
+            </div>
+
+            <div>
+                <p> <i class="fa-solid fa-phone"></i> phone: </p>
+                <span>${user.phone}</span>
+            </div>
+
+            <div>
+                <p> <i class="fa-solid fa-location-dot"></i> address: </p>
+                <span>${user.address.city} - ${user.address.street} - ${user.address.zipcode}</span>
+            </div>
+        </div>
+    `;
+    mainContainer.innerHTML += jsx;
+  });
 };
 
 const init = async () => {
